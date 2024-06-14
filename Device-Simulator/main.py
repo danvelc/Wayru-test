0e86b50395dd6d4e0a5098b5225f997b54804b9b
danvelc <danvelc6@gmail.com>
2024-06-14 16:47:04 -0500

    Almacenamiento con Sqlite

--- Device-Simulator/main.py
+++ Device-Simulator/main.py
@@ -1,6 +1,7 @@
 import requests
 import random
 import time
+from datetime import datetime
 
 url = "http://localhost:8080/data"
 
@@ -14,13 +15,15 @@ while True:
   
   temperatura = round(random.uniform(min_temperatura, max_temperatura), 1)
   humedad = random.randint(min_humedad, max_humedad)
+  fecha_hora_actual = datetime.now()
+  f_formateada = fecha_hora_actual.strftime('%Y-%m-%d %H:%M:%S')
 
-  data = {"temperatura": temperatura, "humedad": humedad}
+  data = {"fecha": f_formateada ,"temperatura": temperatura, "humedad": humedad}
 
   response = requests.post(url, json=data)
 
   if response.status_code == 200:
-    print(f"Envio Exitoso - Temperatura: {temperatura} C, Humedad: {humedad}%")
+    print(f"Envio Exitoso - Fecha: {f_formateada}, Temperatura: {temperatura} C, Humedad: {humedad}%")
   else:
     print(f"Error al enviar data: {response.status_code}")
 
--- Http-Srvr/index.js
+++ Http-Srvr/index.js
@@ -1,31 +1,44 @@
+const sqlite3 = require('sqlite3').verbose();
+let sql;
+
+const db = new sqlite3.Database("./iot-sensors.db", sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message);
+});
+
+sql = 'CREATE TABLE IF NOT EXISTS data(id INTEGER PRIMARY KEY, fecha, temperatura, humedad)';
+db.run(sql);
+
 const express = require('express');
 const app = express();
 const PORT = 8080;
 
-app.listen(PORT,() => console.log(`Activo en http://localhost:${PORT}`)
+app.listen(PORT, () => console.log(`Activo en http://localhost:${PORT}`)
 )
 
 app.use(express.json());
 
-app.get('/data', (req,res) =>{
+app.get('/data', (req, res) => {
     res.status(200).send({
         data: "ejemplo",
         data2: "ejemplo2"
     })
 });
 
-app.post('/data',(req,res)=>{
-    const {temperatura, humedad} = req.body;
+app.post('/data', (req, res) => {
+    const {fecha, temperatura, humedad } = req.body;
 
-    if(!temperatura || !humedad){
-        res.status(400).send({message: 'BAD REQUEST - No se adjuntaron todos los datos'})
+    if (!fecha || !temperatura || !humedad) {
+        res.status(400).send({ message: 'BAD REQUEST - No se adjuntaron todos los datos' })
         return
     }
 
-    console.log({temperatura,humedad})
-
+    console.log({fecha, temperatura, humedad })
+    let command = 'INSERT INTO data(fecha, temperatura, humedad) VALUES (?,?,?)';
+    db.run(command, [fecha, temperatura,humedad], (err) => {
+        if (err) return console.error(err.message);
+    });
     res.send({
-        temperatura, humedad
+        fecha, temperatura, humedad
     })
 });
 
